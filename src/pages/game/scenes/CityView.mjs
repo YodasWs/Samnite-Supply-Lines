efdbb50623ef03d260a0c97b49a8c147f1a7aa1b
krauzX <90498067+krauzX@users.noreply.github.com>
2025-11-28 15:46:44 +0530

    fix: Apply OOP refactoring and null safety fixes

--- src/pages/game/scenes/CityView.mjs
+++ src/pages/game/scenes/CityView.mjs
@@ -1,177 +1,197 @@
-import World from '../../../json/world.mjs';
-import * as Honeycomb from 'honeycomb-grid';
-import * as GameConfig from '../modules/Config.mjs';
+import World from "../../../json/world.mjs";
+import * as Honeycomb from "honeycomb-grid";
+import * as GameConfig from "../modules/Config.mjs";
 
-import City from '../modules/City.mjs';
-import * as Hex from '../modules/Hex.mjs';
-import Tile from '../modules/Tile.mjs';
-import { currentGame } from '../modules/Game.mjs';
+import City from "../modules/City.mjs";
+import * as Hex from "../modules/Hex.mjs";
+import Tile from "../modules/Tile.mjs";
+import { currentGame } from "../modules/Game.mjs";
 
-import InputManager from '../modules/InputManager.mjs';
+import InputManager from "../modules/InputManager.mjs";
 
 let thisCity = null;
 let domProductionQueue = null;
 
 function hideProductionQueue() {
-	if (domProductionQueue) {
-		domProductionQueue.setAttribute('hidden', 'true');
-		domProductionQueue.innerHTML = '';
-	}
+  if (domProductionQueue) {
+    domProductionQueue.setAttribute("hidden", "true");
+    domProductionQueue.innerHTML = "";
+  }
 }
 
 export default {
-	key: 'city-view',
-	preload() {
-	},
-	create(data) {
-		if (!Hex.isHex(data.hex) || !Tile.isTile(data.hex.tile) || !City.isCity(data.hex.city)) {
-			this.scene.resume('mainGameScene');
-			return;
-		}
-		this.scene.pause('mainGameScene');
-		this.scene.moveAbove('mainControls', 'city-view');
-		const windowConfig = GameConfig.getWindowConfig();
+  key: "city-view",
+  preload() {},
+  create(data) {
+    if (
+      !Hex.isHex(data.hex) ||
+      !Tile.isTile(data.hex.tile) ||
+      !City.isCity(data.hex.city)
+    ) {
+      this.scene.resume("mainGameScene");
+      return;
+    }
+    this.scene.pause("mainGameScene");
+    this.scene.moveAbove("mainControls", "city-view");
+    const windowConfig = GameConfig.getWindowConfig();
 
-		thisCity = data.hex.city;
-		domProductionQueue ??= document.getElementById('production-queue');
+    thisCity = data.hex.city;
+    domProductionQueue ??= document.getElementById("production-queue");
 
-		{
-			domProductionQueue.innerHTML = '';
-			const select = document.createElement('select');
-			{
-				const option = document.createElement('option');
-				option.text = '-- Select Unit Type --';
-				option.value = '';
-				select.appendChild(option);
-			}
-			Object.keys(World.units).forEach((unitKey) => {
-				const option = document.createElement('option');
-				option.value = unitKey;
-				option.text = World.units[unitKey].name;
-				select.appendChild(option);
-			});
-			domProductionQueue.appendChild(select);
+    {
+      domProductionQueue.innerHTML = "";
+      const select = document.createElement("select");
+      {
+        const option = document.createElement("option");
+        option.text = "-- Select Unit Type --";
+        option.value = "";
+        select.appendChild(option);
+      }
+      Object.keys(World.units).forEach((unitKey) => {
+        const option = document.createElement("option");
+        option.value = unitKey;
+        option.text = World.units[unitKey].name;
+        select.appendChild(option);
+      });
+      domProductionQueue.appendChild(select);
 
-			const button = document.createElement('button');
-			button.innerText = 'Add to Queue';
-			button.addEventListener('click', () => {
-				thisCity.addToQueue({
-					faction: currentGame.currentPlayer,
-					unitType: select.options[select.selectedIndex].value,
-				});
-			});
-			button.disabled = true;
-			domProductionQueue.appendChild(button);
+      const button = document.createElement("button");
+      button.innerText = "Add to Queue";
+      button.addEventListener("click", () => {
+        thisCity.addToQueue({
+          faction: currentGame.currentPlayer,
+          unitType: select.options[select.selectedIndex].value,
+        });
+      });
+      button.disabled = true;
+      domProductionQueue.appendChild(button);
 
-			select.addEventListener('change', () => {
-				button.disabled = select.options[select.selectedIndex].value === '';
-			});
+      select.addEventListener("change", () => {
+        button.disabled = select.options[select.selectedIndex].value === "";
+      });
 
-			domProductionQueue.appendChild(document.createElement('ul'));
+      domProductionQueue.appendChild(document.createElement("ul"));
 
-			domProductionQueue.removeAttribute('hidden');
-			domProductionQueue.style.zIndex = 1;
-		}
+      domProductionQueue.removeAttribute("hidden");
+      domProductionQueue.style.zIndex = 1;
+    }
 
-		// Start building graphics scene
-		{
-			// Lay black background
-			const graphics = this.add.graphics({ x: 0, y: 0 }).setDepth(0);
-			graphics.fillStyle(0x000000, 0.5);
-			graphics.fillRect(0, 0, windowConfig.width, windowConfig.height);
-		}
-		const graphics = this.add.graphics({ x: 0, y: 0 }).setDepth(1);
+    // Start building graphics scene
+    {
+      // Lay black background
+      const graphics = this.add.graphics({ x: 0, y: 0 }).setDepth(0);
+      graphics.fillStyle(0x000000, 0.5);
+      graphics.fillRect(0, 0, windowConfig.width, windowConfig.height);
+    }
+    const graphics = this.add.graphics({ x: 0, y: 0 }).setDepth(1);
 
-		// Close button
-		graphics.fillStyle(0x000000, 1);
-		graphics.fillRect(windowConfig.width - 100, 0, 100, 100);
-		this.add.text(windowConfig.width - 100, 0, '× ', {
-			fixedWidth: 100,
-			fixedHeight: 100,
-			font: '60pt Trebuchet MS',
-			align: 'right',
-			color: 'white',
-			stroke: 'black',
-			strokeThickness: 7,
-		}).setDepth(2).setInteractive().on('pointerdown', () => {
-			this.scene.stop('city-view');
-		});
+    // Close button
+    graphics.fillStyle(0x000000, 1);
+    graphics.fillRect(windowConfig.width - 100, 0, 100, 100);
+    this.add
+      .text(windowConfig.width - 100, 0, "× ", {
+        fixedWidth: 100,
+        fixedHeight: 100,
+        font: "60pt Trebuchet MS",
+        align: "right",
+        color: "white",
+        stroke: "black",
+        strokeThickness: 7,
+      })
+      .setDepth(2)
+      .setInteractive()
+      .on("pointerdown", () => {
+        this.scene.stop("city-view");
+      });
 
-		// Important constants for translating city tiles locations
-		const [offsetX, offsetY] = [data.hex.x, data.hex.y];
-		// TODO: Need to either make sure tiles fit in screen or that user can pan camera
+    // Important constants for translating city tiles locations
+    const [offsetX, offsetY] = [data.hex.x, data.hex.y];
+    // TODO: Need to either make sure tiles fit in screen or that user can pan camera
 
-		const tileScale = Math.min(windowConfig.height, windowConfig.width) / 7 / GameConfig.tileWidth;
-		const center = {
-			x: windowConfig.width / 2,
-			y: windowConfig.height / 3,
-		};
+    const tileScale =
+      Math.min(windowConfig.height, windowConfig.width) /
+      7 /
+      GameConfig.tileWidth;
+    const center = {
+      x: windowConfig.width / 2,
+      y: windowConfig.height / 3,
+    };
 
-		// Grab and render city hexes
-		Hex.Grid.traverse(Honeycomb.spiral({
-			start: [ data.hex.q, data.hex.r ],
-			radius: 2,
-		})).forEach((hex) => {
-			// Display city hexes
-			// TODO: Basic rendering each hex should be done in one function and then called here and by the global world map. Only further tile details not shown on world map should be added here
-			const tileCenter = {
-				x: (hex.x - offsetX) * tileScale + center.x,
-				y: (hex.y - offsetY) * tileScale + center.y,
-			};
-			const img = this.add.image(tileCenter.x, tileCenter.y, `tile.${hex.terrain.terrain}`).setDepth(1);
-			img.scaleX = tileScale;
-			img.scaleY = tileScale;
-			currentGame.markTerritory(hex, {
-				offsetX: 0 - hex.x + center.x + (hex.x - offsetX) * tileScale,
-				offsetY: 0 - hex.y + center.y + (hex.y - offsetY) * tileScale,
-				graphics: graphics.setDepth(2),
-				lineOffset: 0.95 * tileScale,
-			});
-			// TODO: Show number of laborers on tile
-			// TODO: Show tile improvement
-			// TODO: Allow User to click tile to assign laborers
-			// TODO: Show food production on tile
-			if (hex.tile.laborers.size > 0) {
-				const fixedWidth = GameConfig.tileWidth * tileScale;
-				this.add.text(
-					tileCenter.x - fixedWidth / 2,
-					tileCenter.y + fixedWidth / 4,
-					`Food: ${(hex.terrain.food || 0) + (hex.tile.improvement.food || 0)}`,
-					{
-						font: '14pt Trebuchet MS',
-						align: 'center',
-						color: 'white',
-						stroke: 'black',
-						strokeThickness: 7,
-						fixedWidth,
-					}
-				).setDepth(3);
-			}
-		});
+    // Grab and render city hexes
+    Hex.Grid.traverse(
+      Honeycomb.spiral({
+        start: [data.hex.q, data.hex.r],
+        radius: 2,
+      })
+    ).forEach((hex) => {
+      // Display city hexes
+      // TODO: Basic rendering each hex should be done in one function and then called here and by the global world map. Only further tile details not shown on world map should be added here
+      const tileCenter = {
+        x: (hex.x - offsetX) * tileScale + center.x,
+        y: (hex.y - offsetY) * tileScale + center.y,
+      };
+      const img = this.add
+        .image(tileCenter.x, tileCenter.y, `tile.${hex.terrain.terrain}`)
+        .setDepth(1);
+      img.scaleX = tileScale;
+      img.scaleY = tileScale;
+      currentGame.markTerritory(hex, {
+        offsetX: 0 - hex.x + center.x + (hex.x - offsetX) * tileScale,
+        offsetY: 0 - hex.y + center.y + (hex.y - offsetY) * tileScale,
+        graphics: graphics.setDepth(2),
+        lineOffset: 0.95 * tileScale,
+      });
+      // TODO: Show number of laborers on tile
+      // TODO: Show tile improvement
+      // TODO: Allow User to click tile to assign laborers
+      // TODO: Show food production on tile
+      if (hex.tile.laborers.size > 0) {
+        const fixedWidth = GameConfig.tileWidth * tileScale;
+        this.add
+          .text(
+            tileCenter.x - fixedWidth / 2,
+            tileCenter.y + fixedWidth / 4,
+            `Food: ${
+              (hex.terrain.food || 0) + (hex.tile.improvement.food || 0)
+            }`,
+            {
+              font: "14pt Trebuchet MS",
+              align: "center",
+              color: "white",
+              stroke: "black",
+              strokeThickness: 7,
+              fixedWidth,
+            }
+          )
+          .setDepth(3);
+      }
+    });
 
-		this.inputManager = new InputManager(this);
+    this.inputManager = new InputManager(this);
 
-		this.events.on('sleep', () => {
-			hideProductionQueue();
-			this.scene.wake('mainGameScene');
-		}).on('shutdown', () => {
-			hideProductionQueue();
-			this.scene.wake('mainGameScene');
-		});
-	},
-	update() {
-		if (!thisCity) return;
-		
-		const queueList = document.querySelector('#production-queue > ul');
-		if (!queueList) return;
-		
-		if (queueList.querySelectorAll('li').length !== thisCity.queue.length) {
-			queueList.innerHTML = '';
-			thisCity.queue.forEach(({ faction, unitType }) => {
-				const listItem = document.createElement('li');
-				listItem.innerText = `${faction.name}: ${World.units[unitType].name}`;
-				queueList.appendChild(listItem);
-			});
-		}
-	},
-}
+    this.events
+      .on("sleep", () => {
+        hideProductionQueue();
+        this.scene.wake("mainGameScene");
+      })
+      .on("shutdown", () => {
+        hideProductionQueue();
+        this.scene.wake("mainGameScene");
+      });
+  },
+  update() {
+    if (!thisCity) return;
+
+    const queueList = document.querySelector("#production-queue > ul");
+    if (!queueList) return;
+
+    if (queueList.querySelectorAll("li").length !== thisCity.queue.length) {
+      queueList.innerHTML = "";
+      thisCity.queue.forEach(({ faction, unitType }) => {
+        const listItem = document.createElement("li");
+        listItem.innerText = `${faction.name}: ${World.units[unitType].name}`;
+        queueList.appendChild(listItem);
+      });
+    }
+  },
+};
--- src/pages/game/scenes/TileView.mjs
+++ src/pages/game/scenes/TileView.mjs
@@ -1,153 +1,169 @@
-import * as Honeycomb from 'honeycomb-grid';
-import * as GameConfig from '../modules/Config.mjs';
-import World from '../../../json/world.mjs';
+import * as Honeycomb from "honeycomb-grid";
+import * as GameConfig from "../modules/Config.mjs";
+import World from "../../../json/world.mjs";
 
-import { currentGame } from '../modules/Game.mjs';
-import * as Hex from '../modules/Hex.mjs';
-import Tile from '../modules/Tile.mjs';
+import { currentGame } from "../modules/Game.mjs";
+import * as Hex from "../modules/Hex.mjs";
+import Tile from "../modules/Tile.mjs";
 
-import InputManager from '../modules/InputManager.mjs';
+import InputManager from "../modules/InputManager.mjs";
 
 // A scene function, for example in `create()`
-function displayImageInHTML({
-	htmlElementId,
-	imageKey,
-	scene,
-} = {}) {
-	// Get the HTML <img> element by its ID
-	const imgElement = document.getElementById(htmlElementId);
+function displayImageInHTML({ htmlElementId, imageKey, scene } = {}) {
+  // Get the HTML <img> element by its ID
+  const imgElement = document.getElementById(htmlElementId);
 
-	// Get the Texture instance from the Texture Manager
-	const texture = scene.textures.get(imageKey);
+  // Get the Texture instance from the Texture Manager
+  const texture = scene.textures.get(imageKey);
 
-	if (texture instanceof Phaser.Textures.Texture && imgElement instanceof Element) {
-		// Get the source image from the texture, which is an HTMLImageElement
-		imgElement.append(texture.getSourceImage());
-	}
+  if (
+    texture instanceof Phaser.Textures.Texture &&
+    imgElement instanceof Element
+  ) {
+    // Get the source image from the texture, which is an HTMLImageElement
+    imgElement.append(texture.getSourceImage());
+  }
 }
 
 export default {
-	key: 'tile-view',
-	preload() {
-	},
-	create({ hex }) {
-		if (!Hex.isHex(hex) || !Tile.isTile(hex.tile)) {
-			currentGame.scenes.resume('mainGameScene');
-			return;
-		}
-		this.scene.pause('mainGameScene');
-		const dom = document.getElementById('tile-view');
+  key: "tile-view",
+  preload() {},
+  create({ hex }) {
+    if (!Hex.isHex(hex) || !Tile.isTile(hex.tile)) {
+      currentGame.scenes.resume("mainGameScene");
+      return;
+    }
+    this.scene.pause("mainGameScene");
+    const dom = document.getElementById("tile-view");
 
-		Object.keys(World.laborers).forEach((laborerType) => {
-			this.load.image(`laborers.${laborerType}`, `img/laborers/${laborerType}.png`);
-		});
-		Object.entries(World.improvements).forEach(([key, improvement]) => {
-			if (typeof improvement.tile === 'string' && improvement.tile.length > 0) {
-				this.load.image(`improvements.${key}`, `img/improvements/${improvement.tile}.png`);
-			}
-		});
+    Object.keys(World.laborers).forEach((laborerType) => {
+      this.load.image(
+        `laborers.${laborerType}`,
+        `img/laborers/${laborerType}.png`
+      );
+    });
+    Object.entries(World.improvements).forEach(([key, improvement]) => {
+      if (typeof improvement.tile === "string" && improvement.tile.length > 0) {
+        this.load.image(
+          `improvements.${key}`,
+          `img/improvements/${improvement.tile}.png`
+        );
+      }
+    });
 
-		// Display Terrain Information
-		displayImageInHTML({
-			imageKey: `tile.${hex.terrain.terrain}`,
-			htmlElementId: 'terrain',
-			scene: this,
-		});
-		const elTerrain = dom.querySelector('#terrain');
-		if (elTerrain instanceof Element) {
-			const div = document.createElement('div');
-			div.classList.add('name');
-			div.innerHTML = hex.terrain.name;
-			elTerrain.appendChild(div);
-		}
+    // Display Terrain Information
+    displayImageInHTML({
+      imageKey: `tile.${hex.terrain.terrain}`,
+      htmlElementId: "terrain",
+      scene: this,
+    });
+    const elTerrain = dom.querySelector("#terrain");
+    if (elTerrain instanceof Element) {
+      const div = document.createElement("div");
+      div.classList.add("name");
+      div.innerHTML = hex.terrain.name;
+      elTerrain.appendChild(div);
+    }
 
-		// Display Improvement Information
-		if (hex.tile.improvement) {
-			const elImprovement = dom.querySelector('#improvements');
-			// Create new Phaser canvas
-			const canvas = (() => {
-				if (this.textures.exists('tile-view-improvement')) {
-					return this.textures.get('tile-view-improvement');
-				}
-				return this.textures.createCanvas('tile-view-improvement', GameConfig.tileWidth, GameConfig.tileWidth);
-			})();
-			const elCanvas = canvas.getCanvas();
-			const graphics = canvas.getContext();
-			// Render a white hexagon to the canvas
-			const blandHex = Honeycomb.defineHex({
-				dimensions: GameConfig.tileWidth / 2,
-				orientation: Honeycomb.Orientation.FLAT,
-				origin: 'topLeft',
-			});
-			const oneGrid = new Honeycomb.Grid(blandHex, Honeycomb.rectangle({ width: 1, height: 1 }));
-			const tileHex = oneGrid.getHex({ row: 0, col: 0 });
+    // Display Improvement Information
+    if (hex.tile.improvement) {
+      const elImprovement = dom.querySelector("#improvements");
+      // Create new Phaser canvas
+      const canvas = (() => {
+        if (this.textures.exists("tile-view-improvement")) {
+          return this.textures.get("tile-view-improvement");
+        }
+        return this.textures.createCanvas(
+          "tile-view-improvement",
+          GameConfig.tileWidth,
+          GameConfig.tileWidth
+        );
+      })();
+      const elCanvas = canvas.getCanvas();
+      const graphics = canvas.getContext();
+      // Render a white hexagon to the canvas
+      const blandHex = Honeycomb.defineHex({
+        dimensions: GameConfig.tileWidth / 2,
+        orientation: Honeycomb.Orientation.FLAT,
+        origin: "topLeft",
+      });
+      const oneGrid = new Honeycomb.Grid(
+        blandHex,
+        Honeycomb.rectangle({ width: 1, height: 1 })
+      );
+      const tileHex = oneGrid.getHex({ row: 0, col: 0 });
 
-			graphics.fillStyle = 'white';
-			graphics.beginPath();
-			const [firstCorner, ...otherCorners] = tileHex.corners.map(point => GameConfig.lineShift(
-				{ x: point.x, y: point.y - tileHex.y + 100 },
-				{ x: tileHex.y, y: tileHex.y },
-				0.80,
-			));
+      graphics.fillStyle = "white";
+      graphics.beginPath();
+      const [firstCorner, ...otherCorners] = tileHex.corners.map((point) =>
+        GameConfig.lineShift(
+          { x: point.x, y: point.y - tileHex.y + 100 },
+          { x: tileHex.y, y: tileHex.y },
+          0.8
+        )
+      );
 
-			graphics.moveTo(firstCorner.x, firstCorner.y);
-			otherCorners.forEach(({x, y}) => {
-				graphics.lineTo(x, y);
-			});
-			graphics.closePath();
-			graphics.fill();
+      graphics.moveTo(firstCorner.x, firstCorner.y);
+      otherCorners.forEach(({ x, y }) => {
+        graphics.lineTo(x, y);
+      });
+      graphics.closePath();
+      graphics.fill();
 
-			// Render improvement image to the canvas
-			const img = this.textures.get(`improvements.${hex.tile.improvement.key}`).getSourceImage();
-			const x = (canvas.width - img.width) / 2 - 2;
-			const y = (canvas.height - img.height) / 2;
-			canvas.drawFrame(`improvements.${hex.tile.improvement.key}`, null, x, y);
+      // Render improvement image to the canvas
+      const img = this.textures
+        .get(`improvements.${hex.tile.improvement.key}`)
+        .getSourceImage();
+      const x = (canvas.width - img.width) / 2 - 2;
+      const y = (canvas.height - img.height) / 2;
+      canvas.drawFrame(`improvements.${hex.tile.improvement.key}`, null, x, y);
 
-			// Place canvas into HTML
-			canvas.refresh();
-			elImprovement.appendChild(elCanvas);
+      // Place canvas into HTML
+      canvas.refresh();
+      elImprovement.appendChild(elCanvas);
 
-			// Add improvement name
-			const div = document.createElement('div');
-			div.classList.add('name');
-			div.innerHTML = hex.tile.improvement.title;
-			elImprovement.appendChild(div);
-			console.log('Sam, improvement:', hex.tile.improvement);
-		}
+      // Add improvement name
+      const div = document.createElement("div");
+      div.classList.add("name");
+      div.innerHTML = hex.tile.improvement.title;
+      elImprovement.appendChild(div);
+      console.log("Sam, improvement:", hex.tile.improvement);
+    }
 
-		if (hex.tile.laborers instanceof Set) {
-			const elLaborers = dom.querySelector('#laborers');
-			hex.tile.laborers.forEach((laborer) => {
-				displayImageInHTML({
-					imageKey: `laborers.${laborer.type}`,
-					htmlElementId: 'laborers',
-					scene: this,
-				});
-			});
-		} else {
-			console.log('Sam, no laborers set…');
-		}
+    if (hex.tile.laborers instanceof Set) {
+      const elLaborers = dom.querySelector("#laborers");
+      hex.tile.laborers.forEach((laborer) => {
+        displayImageInHTML({
+          imageKey: `laborers.${laborer.type}`,
+          htmlElementId: "laborers",
+          scene: this,
+        });
+      });
+    } else {
+      console.log("Sam, no laborers set…");
+    }
 
-		// Show Tile View
-		dom.removeAttribute('hidden');
+    // Show Tile View
+    dom.removeAttribute("hidden");
 
-		this.inputManager = new InputManager(this);
+    this.inputManager = new InputManager(this);
 
-		this.events.on('sleep', () => {
-			if (dom) {
-				dom.setAttribute('hidden', true);
-				dom.querySelectorAll('div').forEach((div) => div.innerHTML = '');
-			}
-			this.scene.wake('mainGameScene');
-			this.inputManager?.disableKeyboardInput();
-		}).on('shutdown', () => {
-			if (dom) {
-				dom.setAttribute('hidden', true);
-				dom.querySelectorAll('div').forEach((div) => div.innerHTML = '');
-			}
-			this.scene.wake('mainGameScene');
-		});
-	},
-	update() {
-	},
-}
+    this.events
+      .on("sleep", () => {
+        if (dom) {
+          dom.setAttribute("hidden", true);
+          dom.querySelectorAll("div").forEach((div) => (div.innerHTML = ""));
+        }
+        this.scene.wake("mainGameScene");
+        this.inputManager?.disableKeyboardInput();
+      })
+      .on("shutdown", () => {
+        if (dom) {
+          dom.setAttribute("hidden", true);
+          dom.querySelectorAll("div").forEach((div) => (div.innerHTML = ""));
+        }
+        this.scene.wake("mainGameScene");
+      });
+  },
+  update() {},
+};
